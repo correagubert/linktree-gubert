595855ba6238488596c1b452cb4cfff1b70e9298
correagubert <gubertnicolas@gmail.com>
2025-04-02 21:15:37 -0300

    Criado projeto de linktree.

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { SiRedbull } from "react-icons/si";
+function App() {
+
+  return (
+    <div className='container-app'>
+      <h1>Meus links</h1>
+
+      <a href="http://masterpiece.fansubs.com.br" className='links' target='_blank'>
+      <SiRedbull />
+      Site do meu grupo de traduções
+      <SiRedbull />
+      </a>
+
+    </div>
+  )
+}
+
+export default App
